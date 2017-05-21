1239d518184b2db62b392272fc8790a98271ac8a
ken <ken.de.moor@student.howest.be>
2017-05-21 19:35:35 +0200

    upgrades
    new images, new upgrades, new stuff, total buildings, global mult ...

--- game.js
+++ game.js
@@ -1,9 +1,28 @@
 var gold = 15;
 var totalGold = 15;
 var income = 0;
-var buildings=[[15,1,1,0,"Tent"],[60,2,1,0,"Farm"],[200,3,1,0,"Barracks"],[600,5,1,0,"Church"],[2000,10,1,0,"Castle"],[30000,25,1,0,"Bank"],[1000000,1,1,0,"Builder"]]
+var buildings=[
+[15,1,1,0,"Tent"],
+[60,2,1,0,"Farm"],
+[200,3,1,0,"Barracks"],
+[600,5,1,0,"Church"],
+[2000,10,1,0,"Castle"],
+[30000,25,1,0,"Bank"],
+[1000000,1,1,0,"Builder"]]
+var upgrades=[
+["50 buildings","Get 50 buildings total to unlock",50,"Increase global gold/s by 20%",1.2,5000],
+["100 buildings","Get 100 buildings total to unlock",100,"Increase global gold/s by 20%",1.2,50000],
+["250 buildings","Get 250 buildings total to unlock",250,"Increase global gold/s by 20%",1.2,1000000],
+["1k buildings","Get 1k buildings total to unlock",1000,"Increase global gold/s by 20%",1.2,5000000],
+["10k buildings","Get 10k buildings total to unlock",10000,"Increase global gold/s by 20%",1.2,50000000],
+["100k buildings","Get 100k buildings total to unlock",100000,"Increase global gold/s by 20%",1.2,500000000],
+["1M buildings","Get 1 Million buildings total to unlock",1000000,"Increase global gold/s by 20%",1.2,5000000000],
+["10M buildings","Get 10 Million buildings total to unlock",10000000,"Increase global gold/s by 20%",1.2,50000000000]]
 var tents = 0;
 var lastClicked = 1;
+var globalmult = 1;
+var totalBuildings = 0;
+
 function buyBuilding(building)
 {
 	var buildingID = parseInt(building.id.toString().substr(-1))-1;
@@ -12,6 +31,7 @@ function buyBuilding(building)
 		gold -= buildings[buildingID][0];
 		buildings[buildingID][0] = Math.round(buildings[buildingID][0]*1.13);
 		buildings[buildingID][3] += 1;
+		totalBuildings += 1;
 		if(buildingID<6)
 		{
 			income += parseInt(buildings[buildingID][1]*buildings[buildingID][2]);
@@ -32,43 +52,68 @@ function buyBuilding(building)
 function upgradeClicked(upgrade)
 {
 	var upgradeID = parseInt(upgrade.id.toString().substr(-1));
-	console.log(upgradeID);
+	highlight(upgradeID);
+	setUpgradeText(upgradeID-1);
+}
+
+function highlight(upgradeID)
+{
 	$('#upgrade' + upgradeID).css("box-shadow","0px 0px 35px blue");
 	if(lastClicked!=upgradeID)
 	{
 	$('#upgrade' + lastClicked).css("box-shadow","none");	
 	}
 	lastClicked=upgradeID;
-	
 }
 
+function setUpgradeText(upgradeID)
+{
+	$('#upgradeText').html(
+	'<p>'+upgrades[upgradeID][0]+'</p> '+
+	'<p>'+upgrades[upgradeID][1]+'</p> '+
+	'<p> Progress: <b>'+totalBuildings+'/'+upgrades[upgradeID][2]+'</b></p> '+
+	'<p>'+upgrades[upgradeID][3]+'</p> '+
+	'<button id="buyUpgrade'+(upgradeID+1)+'" class="ui-btn" onclick="buyUpgrade('+upgradeID+')">Buy for '+upgrades[upgradeID][5]+'g</button>'
+	);
+}
+
+function buyUpgrade(upgradeID)
+{
+	if(upgrades[upgradeID][5]<= gold && totalBuildings >= upgrades[upgradeID][2])
+	{
+		globalmult *= upgrades[upgradeID][4];
+		gold -= upgrades[upgradeID][5];
+		$('#upgrade'+(upgradeID+1)).remove();
+		$('#upgradeIcons img:first').click();
+	}
+}
 function cheat()
 {
-	gold += 25*income;
-	totalGold += 25*income;
-	if (totalGold<1000000+income*50)
+	gold += parseInt(25*income*globalmult);
+	totalGold += parseInt(25*income*globalmult);
+	if (totalGold<1000000+income*50*globalmult)
 	{
 		if(totalGold>1000000)
 		{
 		$('#building7').css("display","inline-block");
 		}
-		else if(totalGold>30000)
+		if(totalGold>30000)
 		{
 		$('#building6').css("display","inline-block");
 		}
-		else if(totalGold>2000)
+		if(totalGold>2000)
 		{
 		$('#building5').css("display","inline-block");
 		}
-		else if(totalGold>600)
+		if(totalGold>600)
 		{
 		$('#building4').css("display","inline-block");
 		}
-		else if(totalGold>200)
+		if(totalGold>200)
 		{
 		$('#building3').css("display","inline-block");
 		}
-		else if(totalGold>60)
+		if(totalGold>60)
 		{
 		$('#building2').css("display","inline-block");
 		}
@@ -76,42 +121,44 @@ function cheat()
 }
 setInterval(function(){ 
 tents += 0.1*buildings[6][3]*buildings[6][2];
-gold += income;
-totalGold += income;
+gold += parseInt(income*globalmult);
+totalGold += parseInt(income*globalmult);
 if(tents>=1)
 {
 	buildings[0][3] += Math.floor(tents);
 	income += Math.floor(tents)*parseInt(buildings[0][1]*buildings[0][2]);
+	totalBuildings += Math.floor(tents)
 	tents -= Math.floor(tents);
 	$('#building1 div p:first').html(buildings[0][4]+" x"+buildings[0][3]);
 	$('#building1 div p:nth-child(2)').html(buildings[0][3]*buildings[0][2]*buildings[0][1]+' gold/s');
 	
 }
+setUpgradeText(lastClicked-1);
 $('.info').each(function(i, obj) {
-    obj.innerHTML = "<p>Gold : <b>"+gold+"</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Gold/s</b> : <b>"+income+"</b>"
-	if (totalGold<(1000000+income*2))
+    obj.innerHTML = "<p>Gold : <b>"+gold+"</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Gold/s</b> : <b>"+parseInt(income*globalmult)+"</b>"
+	if (totalGold<(1000000+income*2*globalmult))
 	{
 		if(totalGold>1000000)
 		{
 		$('#building7').css("display","inline-block");
 		}
-		else if(totalGold>30000)
+		if(totalGold>30000)
 		{
 		$('#building6').css("display","inline-block");
 		}
-		else if(totalGold>2000)
+		if(totalGold>2000)
 		{
 		$('#building5').css("display","inline-block");
 		}
-		else if(totalGold>600)
+		if(totalGold>600)
 		{
 		$('#building4').css("display","inline-block");
 		}
-		else if(totalGold>200)
+		if(totalGold>200)
 		{
 		$('#building3').css("display","inline-block");
 		}
-		else if(totalGold>60)
+		if(totalGold>60)
 		{
 		$('#building2').css("display","inline-block");
 		}
