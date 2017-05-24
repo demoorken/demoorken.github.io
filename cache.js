eed45d99954604e6724a6a0e3c82a722e9e55753
ken <ken.de.moor@student.howest.be>
2017-05-24 14:18:21 +0200

    favicons
    more favicons

--- cache.js
+++ cache.js
@@ -13,6 +13,8 @@ self.addEventListener('install', function(e) {
 	   '/jquery.mobile-1.4.5.min.js',
 	   '/jqueryconfirm/jqueryconfirm.js',
 	   '/jqueryconfirm/jqueryconfirm.css',
+	   '/browserconfig.xml',
+	   '/manifest.json',
 	   '/jqueryconfirm/buttons.png',
 	   '/jqueryconfirm/header_bg.jpg',
 	   '/jqueryconfirm/body_bg.jpg',
@@ -30,7 +32,33 @@ self.addEventListener('install', function(e) {
 	   '/images/church.png',
 	   '/images/castle.png',
 	   '/images/bank.png',
-	   '/images/builder.png'
+	   '/images/builder.png',
+	   '/favicon/android-icon-36x36.png',
+	   '/favicon/android-icon-48x48.png',
+	   '/favicon/android-icon-72x72.png',
+	   '/favicon/android-icon-96x96.png',
+	   '/favicon/android-icon-120x120.png',
+	   '/favicon/android-icon-144x144.png',
+	   '/favicon/android-icon-192x192.png',
+	   '/favicon/apple-icon.png',
+	   '/favicon/apple-icon-57x57.png',
+	   '/favicon/apple-icon-60x60.png',
+	   '/favicon/apple-icon-72x72.png',
+	   '/favicon/apple-icon-76x76.png',
+	   '/favicon/apple-icon-114x144.png',
+	   '/favicon/apple-icon-120x120.png',
+	   '/favicon/apple-icon-144x144.png',
+	   '/favicon/apple-icon-152x152.png',
+	   '/favicon/apple-icon-180x180.png',
+	   '/favicon/apple-icon-precomposed.png',
+	   '/favicon/favicon.ico',
+	   '/favicon/favicon-32x32.png',
+	   '/favicon/favicon-96x96.png',
+	   '/favicon/ms-icon-70x70.png',
+	   '/favicon/ms-icon-144x144.png',
+	   '/favicon/ms-icon-150x150.png',
+	   '/favicon/ms-icon-310x310.png'
+	   
      ]);
    })
  );
